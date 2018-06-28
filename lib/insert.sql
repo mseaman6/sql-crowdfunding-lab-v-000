af25665b5783c3cf38d219b6fc93758cdbfad3a9
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-28 20:04:56 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("A", "other", 500, "June 28, 2018", "July 4, 2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("B", "infrastructure", 1000000, "Jan. 1, 2019", "Dec. 31, 2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("C", "more", 453454, "tomorrow", "never");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("D", "dog house", 90, "July 15, 2018", "July 16, 2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("E", "cabinets", 4000, "August 30, 2018", "Nov. 1, 2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("F", "floor", 8000, "May 15, 2018", "June 30, 2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("G", "paint", 600, "July 1, 2018", "July 8, 2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("H", "fixtures" 1000, "July 8, 2018", "July 15, 2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("I", "furniture", 10000, "Dec. 15, 2018", "Dec. 31, 2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("J", "window treatments", 2000, "Nov. 10, 2018", "Nov. 20, 2018");
